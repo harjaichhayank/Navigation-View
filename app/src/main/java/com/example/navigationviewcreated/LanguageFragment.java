098de7d372b8b26864444f524a464620681f769a
Chhayank <harjaichhayank1@gmail.com>
2020-06-08 17:34:13 +0530

    initial commit

--- app/src/main/java/com/example/navigationviewcreated/LanguageFragment.java
+++ app/src/main/java/com/example/navigationviewcreated/LanguageFragment.java
@@ -1,19 +1,11 @@
 package com.example.navigationviewcreated;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LanguageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LanguageFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
--- app/src/main/java/com/example/navigationviewcreated/SearchFragment.java
+++ app/src/main/java/com/example/navigationviewcreated/SearchFragment.java
@@ -8,12 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
